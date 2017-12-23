91fc2047e0d78e9a6f5660ee8182f4fb7e3402b4
melisa014 <mikhailovajk@mail.ru>
2017-12-23 14:03:06 +0300

    Readme и config

--- config.php
+++ config.php
@@ -1,27 +1,27 @@
 <?php
 try {
+    // Включаем полное отображение ошибок
     ini_set( "display_errors", true );
     error_reporting(E_ALL);
+    
     date_default_timezone_set( "Europe/Moscow" );  // http://www.php.net/manual/en/timezones.php
+    
+    // Настройки БД
     define( "DB_DSN", "mysql:host=localhost;dbname=cms;charset=utf8;" );
     define( "DB_USERNAME", "root" );
     define( "DB_PASSWORD", "qwe123" );
+    
+    // Объявление констант, используемых в проекте
     define( "CLASS_PATH", "classes" );
     define( "TEMPLATE_PATH", "templates" );
     define( "HOMEPAGE_NUM_ARTICLES", 5 );
     define( "ADMIN_USERNAME", "admin" );
     define( "ADMIN_PASSWORD", "mypass" );
+    
+    // Подключаем Классы моделей (классы, отвечающие за работу с сущностями базы данных)
     require( CLASS_PATH . "/Article.php" );
     require( CLASS_PATH . "/Category.php" );     
 } catch (Exception $ex) {
-    echo "Sorry, a problem occurred. Please try later.";
+    echo "При загрузке конфигураций возникла проблема!<br><br>";
     error_log( $ex->getMessage() );
 }
-
-//function handleException( $exception ) {
-//    echo "Sorry, a problem occurred. Please try later.";
-//    error_log( $exception->getMessage() );
-//}
-//
-//set_exception_handler( 'handleException' );
-
--- index.php
+++ index.php
@@ -16,17 +16,27 @@ switch ( $action ) {
 }
 
 function archive() {
-    $results = array();
+    $results = [];
+    
     $categoryId = ( isset( $_GET['categoryId'] ) && $_GET['categoryId'] ) ? (int)$_GET['categoryId'] : null;
+    
     $results['category'] = Category::getById( $categoryId );
+    
     $data = Article::getList( 100000, $results['category'] ? $results['category']->id : null );
+    
     $results['articles'] = $data['results'];
     $results['totalRows'] = $data['totalRows'];
+    
     $data = Category::getList();
     $results['categories'] = array();
-    foreach ( $data['results'] as $category ) $results['categories'][$category->id] = $category;
+    
+    foreach ( $data['results'] as $category ) {
+        $results['categories'][$category->id] = $category;
+    }
+    
     $results['pageHeading'] = $results['category'] ?  $results['category']->name : "Article Archive";
     $results['pageTitle'] = $results['pageHeading'] . " | Widget News";
+    
     require( TEMPLATE_PATH . "/archive.php" );
 }
 
@@ -49,28 +59,16 @@ function homepage() {
     $results['articles'] = $data['results'];
     $results['totalRows'] = $data['totalRows'];
     
-//    echo "<pre>"; 
-//    print_r ($results['articles'][2]->publicationDate);
-//    echo "</pre>";  
-    //Обращаемся к дате массива $results. Дата = 0
-    
-//    echo "<pre>";
-//    print_r($results);
-//    echo "</pre>";
-//    Здесь массив $results пустой
     $data = Category::getList();
     $results['categories'] = array();
     foreach ( $data['results'] as $category ) $results['categories'][$category->id] = $category;
     $results['pageTitle'] = "Widget News";
+    
 //    echo "<pre>";
-//    print_r($results);
+//    print_r($data);
 //    echo "</pre>";
-    // Здесб есть всего 5 записей, исключая ту, у которой нет категории
+//    die();
+    
     require( TEMPLATE_PATH . "/homepage.php" );
     
-    
 }
-	  
-	
-//          echo '<pre>'; print_r($results); echo '</pre>';
-//          die();
